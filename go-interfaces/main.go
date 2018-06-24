c2f42da6039e417887755dd423b5872995f3f0e7
Richard Orofeo <chardy.orofeo@yahoo.com.ph>
2018-06-24 21:09:26 +0800

    Added implementation of interfaces

---
+++ go-interfaces/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type Bot interface {
+	getGreeting() string
+}
+type englishBot struct {
+}
+type spanishBot struct {
+}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func (englishBot) getGreeting() string {
+	return "Hi There!"
+}
+func (spanishBot) getGreeting() string {
+	return "Ola!"
+}
+
+func printGreeting(b Bot) {
+	fmt.Println(b.getGreeting())
+}
